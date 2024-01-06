6c0d9f8e69406f73b33e4ca5f0ea283ed6c8e65d
lsileoni <lsileoni@proton.me>
2024-01-06 18:53:12 +0200

    Made modification to main which includes basic testing for tini

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,39 @@
 #include "Log.hpp"
 #include "Config.hpp"
 #include "Server.hpp"
+#include "TiniTree.hpp"
 
 int main()
 {
     try
     {
         Config  config;
-        Server  server(config);
+        TiniTree tree;
+        TiniNode& root = tree.getRoot();
 
+        INFO("Printing the whole tree");
+        // Printing the whole tree
+        root.printContents(0, "");
+
+        std::cout << "\n\n\n";
+        INFO("Printing a nested map");
+        // Printing a nested map
+        root["a"]["b"]["c"].printContents(0, "");
+
+        std::cout << "\n\n\n";
+        INFO("Printing the server listing");
+        // Printing servers
+        for (auto n : root["http"]["servers"].getVectorValue())
+            n->printContents(0, "");
+
+        std::cout << "\n\n\n";
+        INFO("Printing an individual key value pair");
+        // Printing individual key-value pair
+        auto v = root["root_map_value"].getStringValue();
+        std::cout << "Value for key " << "root_map_value is: " << v << std::endl;
+        
+        Server  server(config);
+        
         Runtime::instance().run();
     }
     catch (const char *e)
