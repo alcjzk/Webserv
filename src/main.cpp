18c578c87466bc803cfb3c1fa28e681c911c1ea8
lsileoni <lsileoni@proton.me>
2024-01-06 18:56:45 +0200

    Removed dubious comments

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,19 @@ int main()
         TiniNode& root = tree.getRoot();
 
         INFO("Printing the whole tree");
-        // Printing the whole tree
         root.printContents(0, "");
 
         std::cout << "\n\n\n";
         INFO("Printing a nested map");
-        // Printing a nested map
         root["a"]["b"]["c"].printContents(0, "");
 
         std::cout << "\n\n\n";
         INFO("Printing the server listing");
-        // Printing servers
         for (auto n : root["http"]["servers"].getVectorValue())
             n->printContents(0, "");
 
         std::cout << "\n\n\n";
         INFO("Printing an individual key value pair");
-        // Printing individual key-value pair
         auto v = root["root_map_value"].getStringValue();
         std::cout << "Value for key " << "root_map_value is: " << v << std::endl;
 
