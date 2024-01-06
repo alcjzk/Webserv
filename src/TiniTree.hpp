5895578e257c615fdccf4b3423ee2eee30b4cb27
lsileoni <lsileoni@proton.me>
2024-01-06 18:52:44 +0200

    Added the TiniTree class

---
+++ src/TiniTree.hpp
@@ -0,0 +1,31 @@
+#include "TiniNode.hpp"
+#include "TiniUtils.hpp"
+#include "TiniValidator.hpp"
+#include <vector>
+
+#ifndef TINITREE_HPP
+# define TINITREE_HPP
+
+class TiniTree
+{
+    private:
+        int             _ctx;
+        TiniNode*       _current;
+        std::string     _filename;
+        TiniValidator   _validator;
+        std::vector<std::string> _split_input;
+    public:
+        TiniNode*       _root;
+        TiniTree();
+        ~TiniTree();
+        TiniTree(std::string config_location);
+        TiniTree(const TiniTree& other) = default;
+        TiniTree& operator=(const TiniTree& other) = default;
+
+        void constructTree();
+        int contextSwitch(std::string ctx, int row);
+        int valueInsertion(std::string act, int row);
+        TiniNode& getRoot();
+};
+
+#endif
