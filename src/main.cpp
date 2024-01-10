b3b15175ff97f4e471c583d3af5455d0513fc6d5
lsileoni <lsileoni@proton.me>
2024-01-10 10:39:46 +0200

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,46 @@
-#include <iostream>
-#include <cstdlib>
-#include <exception>
-#include "Log.hpp"
 #include "Config.hpp"
+#include "Log.hpp"
 #include "Server.hpp"
 #include "TiniTree.hpp"
+#include <cstdlib>
+#include <exception>
+#include <iostream>
 
-int main()
-{
-    try
-    {
-        Config  config;
-        TiniTree tree;
-        TiniNode& root = tree.getRoot();
+int main() {
+  try {
+    Config config;
+    TiniTree tree;
+    TiniNode &root = tree.getRoot();
+    TiniNode test = root["http"];
+    TiniNode funny = test;
 
-        INFO("Printing the whole tree");
-        root.printContents(0, "");
+    INFO("Printing the whole tree");
+    root.printContents(0, "");
 
-        std::cout << "\n\n\n";
-        INFO("Printing a nested map");
-        root["a"]["b"]["c"].printContents(0, "");
+    std::cout << "\n\n\n";
+    INFO("Printing a nested map");
+    root["a"]["b"]["c"].printContents(0, "");
 
-        std::cout << "\n\n\n";
-        INFO("Printing the server listing");
-        for (auto n : root["http"]["servers"].getVectorValue())
-            n->printContents(0, "");
+    std::cout << "\n\n\n";
+    INFO("Printing the server listing");
+    for (auto n : root["http"]["servers"].getVectorValue())
+      n->printContents(0, "");
 
-        std::cout << "\n\n\n";
-        INFO("Printing an individual key value pair");
-        auto v = root["root_map_value"].getStringValue();
-        std::cout << "Value for key " << "root_map_value is: " << v << std::endl;
+    std::cout << "\n\n\n";
+    INFO("Printing an individual key value pair");
+    auto v = root["root_map_value"].getStringValue();
+    std::cout << "Value for key "
+              << "root_map_value is: " << v << std::endl;
 
-        Server  server(config);
+    Server server(config);
 
-        Runtime::instance().run();
-    }
-    catch (const char *e)
-    {
-        ERR(e);
-        return EXIT_FAILURE;
-    }
-    catch (const std::exception& e)
-    {
-        ERR(e.what());
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+    Runtime::instance().run();
+  } catch (const char *e) {
+    ERR(e);
+    return EXIT_FAILURE;
+  } catch (const std::exception &e) {
+    ERR(e.what());
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
