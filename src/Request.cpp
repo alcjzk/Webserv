4d769372a0c1c465e1c025889cbfd173f1b882c9
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-11 14:26:43 +0200

    Impl Request::into_response

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,23 +1,57 @@
 #include <algorithm>
+#include "Log.hpp"
+#include "Server.hpp"
+#include "FileResponse.hpp"
+#include "HTTPError.hpp"
 #include "Request.hpp"
 
 using std::string;
 using std::vector;
 
-Response* Request::into_response() const
+Response* Request::into_response(const Server& server) const
 {
-    // TODO: Implement responses based on the request
-    return new TextResponse("Got request");
+    auto routes = server.routes();
+
+    INFO(_request_line.request_target());
+
+    auto host = header("Host");
+    if (host == _headers.end())
+    {
+        throw HTTPError(Status::BAD_REQUEST);
+    }
+
+    URI request_uri(_request_line.request_target(), host->_value);
+
+    INFO(request_uri.path());
+
+    auto route =
+        std::find_if(routes.cbegin(), routes.cend(),
+                     [request_uri](const Route& route) { return route.match(request_uri.path()); });
+
+    if (route == routes.cend())
+    {
+        throw HTTPError(Status::BAD_REQUEST);
+    }
+
+    auto target = route->map(request_uri.path());
+    INFO("Target resource: " << target);
+
+    auto status = std::filesystem::status(target);
+    if (status.type() == std::filesystem::file_type::not_found)
+    {
+        throw HTTPError(Status::NOT_FOUND);
+    }
+    if (status.type() != std::filesystem::file_type::regular)
+    {
+        throw HTTPError(Status::FORBIDDEN);
+    }
+
+    return new FileResponse(target);
 }
 
 const Method& Request::method() const
 {
     return _request_line.method();
-}
-
-const URI& Request::uri() const
-{
-    return _request_line.uri();
 }
 
 const HTTPVersion& Request::http_version() const
--- src/Request.hpp
+++ src/Request.hpp
@@ -6,17 +6,18 @@
 #include "Response.hpp"
 #include "Header.hpp"
 
+class Server;
+
 class Request
 {
     public:
         Request() = default;
 
         const Method&                       method() const;
-        const URI&                          uri() const;
         const HTTPVersion&                  http_version() const;
         std::vector<Header>::const_iterator header(const std::string& name) const;
 
-        Response*                           into_response() const;
+        Response*                           into_response(const Server& server) const;
 
         RequestLine                         _request_line;
         std::vector<Header>                 _headers;
