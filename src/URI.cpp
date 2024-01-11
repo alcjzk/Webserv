5dcd818d2d69e66a958d925d387f00fe4f12128f
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-01-11 14:25:18 +0200

    Initial URI impl

--- src/URI.cpp
+++ src/URI.cpp
@@ -5,14 +5,40 @@ using std::string;
 
 URI::URI() {}
 
-URI::URI(string value) : _value(value) {}
-
-const string& URI::value() const
+const std::string& URI::host() const
 {
-    return _value;
+    return _host;
+}
+
+const std::string& URI::path() const
+{
+    return _path;
+}
+
+const std::string& URI::query() const
+{
+    return _query;
+}
+
+URI::URI(string request_target, string host) : _host(host)
+{
+    size_t end;
+    size_t start;
+
+    end = request_target.find_first_of('?');
+    _path = request_target.substr(0, end);
+    if (end != string::npos)
+    {
+        _query = request_target.substr(end + 1);
+    }
 }
 
 ostream& operator<<(ostream& os, const URI& uri)
 {
-    return os << uri.value();
+    os << "http://" << uri.host();
+    if (!uri.query().empty())
+    {
+        os << uri.query();
+    }
+    return os;
 }
--- src/URI.hpp
+++ src/URI.hpp
@@ -7,12 +7,20 @@ class URI
 {
     public:
         URI();
-        URI(std::string uri);
+        // URI(std::string uri); // From request target and host
+        URI(std::string request_target, std::string host);
 
-        const std::string& value() const;
+        // const std::string& value() const;
+
+        const std::string& host() const;
+        const std::string& path() const;
+        const std::string& query() const;
 
     private:
-        std::string _value;
+        std::string _host;  // Match server host
+        std::string _path;  // Match resource
+        std::string _query; // Passed
+                            // std::string _scheme; // Probably unnecessary
 };
 
 std::ostream& operator<<(std::ostream& os, const URI& uri);
