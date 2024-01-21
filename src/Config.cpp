5f64085ab9fa7a180b873798542aa678936f9834
lsileoni <lsileoni@proton.me>
2024-01-21 02:45:14 +0200

    Config update

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,30 +1,41 @@
 #include "Config.hpp"
+#include "HostAttributes.hpp"
+#include "Log.hpp"
+#include <stdexcept>
 
 using std::string;
 
-Config::Config()
-    : _port("8000"), _backlog(128) // OSX capped value for listen(2)
+Config::Config(std::map<std::string, TiniNode*>& server, std::map<std::string, TiniNode*>& root)
+    : _port(string("8000")), _backlog(128), _body_size(4096) // OSX capped value for listen(2)
 {
-}
+    TiniNode* body_size = root["body_size"];
+    TiniNode* port = server["port"];
 
-Config::Config(std::string port)
-    : _port(string(port)), _backlog(128) // OSX capped value for listen(2)
-{
-}
-
-Config::Config(TiniNode& server)
-    : _port(string()), _backlog(128) // OSX capped value for listen(2)
-{
     try
     {
-        std::map<std::string, TiniNode*>& s = server.getMapValue();
-        _port = s["port"]->getStringValue();
-        std::cout << "port is " << _port << std::endl;
+        for (const auto& [key, value] : server)
+        {
+            if (value && value->getType() == TiniNode::T_MAP)
+                _attrs.push_back(HostAttributes(key, value));
+        }
     }
     catch (...)
     {
-        throw std::runtime_error("Configuration parsing error!");
+        throw std::runtime_error("Server is not defined as map!");
     }
+
+    if (!body_size || body_size->getType() != TiniNode::T_STRING)
+    {
+        INFO("Body size not specified or invalid type, defaulting to 4096");
+    }
+    else
+        _body_size = stoi(body_size->getStringValue());
+    if (!port || port->getType() != TiniNode::T_STRING)
+    {
+        INFO("Port not specified or invalid type, defaulting to 8000");
+    }
+    else
+        _port = port->getStringValue();
 }
 
 const string& Config::port() const
@@ -35,4 +46,9 @@ const string& Config::port() const
 int Config::backlog() const
 {
     return _backlog;
+}
+
+const std::vector<HostAttributes>& Config::attrs() const
+{
+    return _attrs;
 }
--- src/Config.hpp
+++ src/Config.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
 #include <string>
 #include "TiniTree.hpp"
+#include "Route.hpp"
+#include "HostAttributes.hpp"
 
 class Config
 {
     public:
-        Config();
-        Config(std::string port);
-        Config(TiniNode& node);
+        Config() = delete;
+        Config(std::map<std::string, TiniNode*>& server, std::map<std::string, TiniNode*>& root);
 
-        const std::string& port() const;
-        int                             backlog() const;
+        const std::string&                 port() const;
+        int                                backlog() const;
+        const std::vector<HostAttributes>& attrs() const;
 
-    private:
-        std::string _port;
-        int                      _backlog;
+    protected:
+        std::string                 _port;
+        std::vector<HostAttributes> _attrs;
+        int                         _backlog;
+        size_t                      _body_size;
 };
