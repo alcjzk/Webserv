7259f757d84b0211c9e22f7db559c4881d79e067
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-02-25 05:32:11 +0200

    Removed unused include & scope decl

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include "Task.hpp"
 #include "Log.hpp"
-#include "Runtime.hpp"
 
 Task::Task(int fd, WaitFor wait_for, std::optional<TimePoint> expire_time)
     : _fd(fd), _wait_for(wait_for), _is_complete(false),
@@ -22,7 +21,7 @@ bool Task::is_complete() const
     return _is_complete;
 }
 
-bool Task::is_expired_at(Task::TimePoint time_point) const
+bool Task::is_expired_at(TimePoint time_point) const
 {
     if (_expire_time && *_expire_time <= time_point)
         return true;
