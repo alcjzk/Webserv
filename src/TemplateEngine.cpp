dbcc2def2fa929630d5be80faf1de0db28cc8322
lsileoni <lsileoni@proton.me>
2024-03-08 07:24:58 +0200

    Template engine mutliple match fix

--- src/TemplateEngine.cpp
+++ src/TemplateEngine.cpp
@@ -9,9 +9,9 @@ void TemplateEngine::set_value(const std::string& key, const std::string& value)
 
 std::string TemplateEngine::render()
 {
+    size_t               offset = 0;
     std::string          rendered = _template_str;
     std::regex           pattern(R"(\{\{(\w+)\}\})");
-
     std::sregex_iterator begin(rendered.begin(), rendered.end(), pattern);
     std::sregex_iterator end;
 
@@ -19,9 +19,11 @@ std::string TemplateEngine::render()
     {
         std::smatch match = *begin;
         std::string key = match[1].str();
-
         if (_data.find(key) != _data.end())
-            rendered.replace(match.position(), match.length(), _data[key]);
+        {
+            rendered.replace(match.position() + offset, match.length(), _data[key]);
+            offset += _data[key].size() - match.length();
+        }
         ++begin;
     }
 
