e4f15cbbd37499996ceba1690013ff1eb1db5a60
Tuomas <tuomas.jaasalo@alceon.fi>
2024-03-09 20:34:03 +0200

    Added missing try-catch

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <stdexcept>
 #include "Log.hpp"
 #include "Server.hpp"
 #include "FileResponse.hpp"
@@ -12,51 +13,58 @@ using std::string;
 
 Response* Request::into_response(const Server& server) const
 {
-    const Header* host = header("Host");
-    if (!host)
-        throw HTTPError(Status::BAD_REQUEST);
-
-    HttpUri      uri(_request_line.request_target(), host->_value);
-
-    const Route* route = server.route(uri.path(), uri.host());
-    if (!route)
-        throw HTTPError(Status::BAD_REQUEST);
-
-    // Redirect does not depend on request_uri mapping to a valid target directory on the host
-    if (route->_type == Route::REDIRECTION)
-        return new RedirectionResponse(route->_redir.value());
-
-    Path target = route->map(uri.path());
-
-    if (!route->method_get())
-        throw HTTPError(Status::FORBIDDEN);
-
-    if (target.type() == Path::Type::NOT_FOUND)
-        throw HTTPError(Status::NOT_FOUND);
-
-    // Can add CGI suffix parsing here later
-    if (target.type() == Path::Type::REGULAR)
-        return new FileResponse(target);
-
-    if (target.type() != Path::Type::DIRECTORY)
-        throw HTTPError(Status::FORBIDDEN);
-
-    // At this point we know it's a directory, can remove a layer of nesting
     try
     {
-        if (route->_default_file.has_value())
-            return new FileResponse(target + Path(route->_default_file.value()));
+        const Header* host = header("Host");
+        if (!host)
+            throw HTTPError(Status::BAD_REQUEST);
+
+        HttpUri      uri(_request_line.request_target(), host->_value);
+        const Route* route = server.route(uri.path(), uri.host());
+        if (!route)
+            throw HTTPError(Status::BAD_REQUEST);
+
+        // Redirect does not depend on request_uri mapping to a valid target directory on the host
+        if (route->_type == Route::REDIRECTION)
+            return new RedirectionResponse(route->_redir.value());
+
+        Path target = route->map(uri.path());
+
+        if (!route->method_get())
+            throw HTTPError(Status::FORBIDDEN);
+
+        if (target.type() == Path::Type::NOT_FOUND)
+            throw HTTPError(Status::NOT_FOUND);
+
+        // Can add CGI suffix parsing here later
+        if (target.type() == Path::Type::REGULAR)
+            return new FileResponse(target);
+
+        if (target.type() != Path::Type::DIRECTORY)
+            throw HTTPError(Status::FORBIDDEN);
+
+        // At this point we know it's a directory, can remove a layer of nesting
+        try
+        {
+            if (route->_default_file.has_value())
+                return new FileResponse(target + Path(route->_default_file.value()));
+        }
+        catch (const std::exception& e)
+        {
+            INFO("Request::into_response: " << e.what());
+            if (server.map_attributes(uri.host()).dirlist())
+                return new DirectoryResponse(target, uri.path());
+            throw HTTPError(Status::NOT_FOUND);
+        }
+        if (!server.map_attributes(uri.host()).dirlist())
+            throw HTTPError(Status::FORBIDDEN);
+        return new DirectoryResponse(target, uri.path());
     }
-    catch (const std::exception& e)
+    catch (const std::runtime_error& error)
     {
-        INFO("Request::into_response: " << e.what());
-        if (server.map_attributes(uri.host()).dirlist())
-            return new DirectoryResponse(target, uri.path());
-        throw HTTPError(Status::NOT_FOUND);
+        WARN("Request::into_response: " << error.what());
+        throw HTTPError(Status::BAD_REQUEST);
     }
-    if (!server.map_attributes(uri.host()).dirlist())
-        throw HTTPError(Status::FORBIDDEN);
-    return new DirectoryResponse(target, uri.path());
 }
 
 const Method& Request::method() const
