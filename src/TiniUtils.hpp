676c71909746b5c1ed63b81edc51e163ea120f6d
lsileoni <lsileoni@proton.me>
2024-03-09 18:07:46 +0200

    Namespaces and tests for the tiniutils header

--- src/TiniUtils.hpp
+++ src/TiniUtils.hpp
@@ -1,15 +1,24 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-#ifndef TINI_UTILS_HPP
-#define TINI_UTILS_HPP
+namespace tiniutils
+{
+    std::vector<std::string> split(std::string raw_string, std::string delimiter, char escape = '\0');
+    int                      match_input(std::string input);
+    int                      chr_to_ctx_state(char c, char p);
+    int                      chr_to_act_state(char c);
+    std::vector<std::string> removeSpaces(std::vector<std::string> split_input);
+    void                     printErr(int err_type, int col, int row, std::string line);
+}
 
-std::vector<std::string> split(std::string s, std::string d);
-int                      match_input(std::string input);
-int                      chr_to_ctx_state(char c);
-int                      chr_to_act_state(char c);
-std::vector<std::string> removeSpaces(std::vector<std::string> split_input);
-void                     printErr(int err_type, int col, int row, std::string line);
-
-#endif
+namespace tiniutilsTest
+{
+    void              split_basic_test();
+    void              split_weird_delim_test();
+    void              split_multichar_delim_test();
+    void              split_none_test();
+    void              split_basic_escape_test();
+}
