1969b46cda31b9cc437672343957305944a09c68
Tuomas <tuomas.jaasalo@alceon.fi>
2024-03-10 22:36:24 +0200

    File impl improvements
    + Allowed default construction & move assignment
    + Extended doccomments

--- src/File.cpp
+++ src/File.cpp
@@ -9,23 +9,25 @@ using std::ostream;
 
 File::~File()
 {
-    if (_fd)
-    {
-        if (close(*_fd) != 0)
-        {
-            WARN("fd '" << *_fd << "' did not close properly: " << strerror(errno));
-        }
-        INFO("closed fd '" << *_fd << "'");
-    }
+    this->close();
 }
 
-File::File(int fd) : _fd(fd) {}
+File::File(int fd) noexcept : _fd(fd) {}
 
 File::File(File&& other) noexcept : _fd(std::exchange(other._fd, std::nullopt)) {}
 
+File& File::operator=(File&& other) noexcept
+{
+    this->close();
+    _fd = std::exchange(other._fd, std::nullopt);
+    return *this;
+}
+
 File::operator int() const
 {
-    return *_fd;
+    if (_fd.has_value())
+        return *_fd;
+    return -1;
 }
 
 int File::fd() const
@@ -36,6 +38,18 @@ int File::fd() const
 int File::take_fd()
 {
     return std::exchange(_fd, std::nullopt).value();
+}
+
+void File::close()
+{
+    if (_fd)
+    {
+        if (::close(*_fd) != 0)
+        {
+            WARN("fd '" << *_fd << "' did not close properly: " << strerror(errno));
+        }
+        INFO("closed fd '" << *_fd << "'");
+    }
 }
 
 ostream& operator<<(ostream& os, const File& file)
--- src/File.hpp
+++ src/File.hpp
@@ -8,31 +8,50 @@
 class File
 {
     public:
-        /// Destructs the type and closes the associated file descriptor.
+        /// Destructs the type and closes any associated file descriptor.
         ~File();
 
+        /// Constructs the type without an associated file descriptor.
+        File() noexcept = default;
+
         /// Constructs the type from a raw file descriptor.
-        File(int fd);
+        File(int fd) noexcept;
 
         /// Constructs the type by moving.
         File(File&&) noexcept;
 
-        /// Conversion to raw file descriptor.
+        /// Move assings from other. If `this` is already associated with a file descriptor, it is
+        /// closed before the replacement occurs.
+        File& operator=(File&& other) noexcept;
+
+        /// Implicit conversion to raw file descriptor. If there is no associated file descriptor,
+        /// -1 is returned.
+        ///
+        /// This conversion exists for better compatability with libc functions. The throwing
+        /// alternative `fd()` is a better alternative, when `File` is known to contain an
+        /// associated fd.
         operator int() const;
 
         /// Returns a copy of the raw file descriptor. Same as `operator int()`.
-        int fd() const;
+        ///
+        /// @throws `std::bad_optional_access` if there is no associated file descriptor.
+        int                  fd() const;
 
         /// Returns ownership of the raw file descriptor. This allows dropping the wrapper without
         /// closing the fd.
-        int take_fd();
+        ///
+        /// @throws `std::bad_optional_access` if there is no associated file descriptor.
+        int                  take_fd();
 
-        File() = delete;
-        File(const File&) = delete;
-        File&                operator=(const File&) = delete;
-
+        /// Inserts a text representation of `File` into the output stream.
         friend std::ostream& operator<<(std::ostream& os, const File& file);
 
+        // Disallow copy sematics.
+        File(const File&) = delete;
+        File& operator=(const File&) = delete;
+
     private:
+        void               close();
+
         std::optional<int> _fd;
 };
