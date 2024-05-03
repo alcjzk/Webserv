79a10d6f0744c0e8637d613800f49b8fe1455a3a
lsileoni <lsileoni@proton.me>
2024-05-03 14:04:46 +0300

    Extracting relevant files

--- src/RedirectionResponse.cpp
+++
@@ -1,6 +0,0 @@
-#include "RedirectionResponse.hpp"
-
-RedirectionResponse::RedirectionResponse(const std::string value, Status status) : Response(status)
-{
-    header(Header("Location", value));
-}
