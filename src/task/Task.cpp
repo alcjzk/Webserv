f5a1a681f3e364491a88aad3decc4ebc7acfdc6e
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-05-06 20:51:26 +0300

    Removed unused operator def from Task

--- src/task/Task.cpp
+++ src/task/Task.cpp
@@ -1,10 +1,5 @@
 #include "Task.hpp"
 
-bool Task::operator==(int fd)
-{
-    return this->fd() == fd;
-}
-
 bool Task::is_expired_at(TimePoint time_point) const
 {
     if (this->expire_time() && *(this->expire_time()) <= time_point)
