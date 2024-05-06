5ce208d314a3d9f1c07e311977041184c1b98b43
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2024-05-06 20:37:01 +0300

    Throw on attempt to construct HTTPError from non-error

--- src/http/HTTPError.hpp
+++ src/http/HTTPError.hpp
@@ -6,6 +6,9 @@
 class HTTPError : public std::exception
 {
     public:
+        /// Construct from a HTTP status.
+        ///
+        /// @throws std::logic_error - status is not an error.
         HTTPError(Status status);
 
         virtual const char* what() const noexcept;
