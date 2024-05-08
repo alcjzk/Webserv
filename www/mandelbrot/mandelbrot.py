9332ac0a6e0bbdb5882504091e5fa0d1481a56f0
lsileoni <lsileoni@proton.me>
2024-05-08 10:29:54 +0300

    Add mandelbrot.py example cgi script

---
+++ www/mandelbrot/mandelbrot.py
@@ -0,0 +1,117 @@
+import cmath
+import os
+import cgi
+import time
+
+form = cgi.FieldStorage()
+
+def brot(c):
+    itmax = 42
+    z = complex(0, 0)
+    while (z.real < 5.0 and z.imag < 5.0 and itmax > 0):
+        z = z**2 + c
+        itmax -= 1
+    return itmax;
+
+if __name__ == "__main__":
+
+    qs = os.environ['QUERY_STRING']
+
+    print("""
+          <!DOCTYPE HTML>
+            <html>
+              <head>
+            </head>
+            <body>
+            <pre>
+          """)
+    print(f'QUERY_STRING: {qs}')
+
+    d = {}
+    vals = qs.split('&')
+    print(f'vals: {vals}')
+    for val in vals:
+        if '=' not in val:
+            continue
+        key_val = val.split('=')
+        d[key_val[0]] = float(key_val[1])
+    print(f'd: {d}')
+    print('form', form)
+    if form.getvalue('x_left') is not None:
+        x_left = float(form.getvalue('x_left'))
+    else:
+        x_left = -1.5
+
+    if form.getvalue('x_right') is not None:
+        x_right = float(form.getvalue('x_right'))
+    else:
+        x_right = 0.5
+
+    if form.getvalue('y_bottom') is not None:
+        y_bottom = float(form.getvalue('y_bottom'))
+    else:
+        y_bottom = -1.0
+
+    if form.getvalue('y_top') is not None:
+        y_top = float(form.getvalue('y_top'))
+    else:
+        y_top = 1.0
+
+    if form.getvalue('x_res') is not None:
+        x_res = float(form.getvalue('x_res'))
+    else:
+        x_res = 1.0
+    if form.getvalue('y_res') is not None:
+        y_res = float(form.getvalue('y_res'))
+    else:
+        y_res = 1.0
+
+    start = complex(x_left, y_top)
+    end = complex(x_right, y_bottom)
+
+    x_step = abs(x_left - x_right) / x_res
+    y_step = abs(y_bottom - y_top) / y_res
+    print(f'x_left: {x_left}     x_right: {x_right}')
+    print(f'y_bottom: {y_bottom} y_top: {y_top}')
+    print(f'x_step: {x_step}     y_step: {y_step}')
+    print(f'x_res: {x_res}       y_res: {y_res}')
+
+    y_inc = y_top
+    while y_inc > y_bottom:
+        x_inc = x_left
+        while x_inc < x_right:
+            if brot(complex(x_inc, y_inc)):
+                print("#", end="")
+            else:
+                print(" ", end="")
+            x_inc += x_step
+            # print(f'x_inc: {x_inc}     y_inc: {y_inc}')
+        print()
+        y_inc -= y_step
+    print("</pre>")
+    print(f"""
+        <form action="/mandelbrot/mandelbrot.py" method="POST">
+
+            <label for="x_left">x_left:</label><br>
+            <input type="number" id="x_left" name="x_left" value="{x_left}"><br>
+            <label for="x_right">x_right:</label><br>
+            <input type="number" id="x_right" name="x_right" value="{x_right}"><br><br>
+
+            <label for="y_top">y_top:</label><br>
+            <input type="number" id="y_top" name="y_top" value="{y_top}"><br><br>
+            <label for="y_bottom">y_bottom:</label><br>
+            <input type="number" id="y_bottom" name="y_bottom" value="{y_bottom}"><br><br>
+
+            <label for="x_res">x_res:</label><br>
+            <input type="number" id="x_res" name="x_res" value="{x_res}"><br>
+            <label for="y_res">y_res:</label><br>
+            <input type="number" id="y_res" name="y_res" value="{y_res}"><br><br>
+
+            <input type="submit" value="Submit">
+        </form>
+
+          """)
+    print("""
+        </body>
+        </html>
+          """)
