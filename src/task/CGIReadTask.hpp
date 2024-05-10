5c13fd1b7b8e6c8598c4bc9ef85d1df8883c576e
Tuomas <tuomas.jaasalo@alceon.fi>
2024-05-10 18:17:54 +0300

    Cleanup CGIReadTask

--- src/task/CGIReadTask.hpp
+++ src/task/CGIReadTask.hpp
@@ -13,44 +13,32 @@
 class CGIReadTask : public BasicTask
 {
     public:
+        CGIReadTask(int read_end, const Config& config, Child&& pid);
+
+        virtual void                   run() override;
+        virtual void                   abort() override;
+        virtual std::optional<Seconds> expire_time() const override;
+
+        // Returns true if the task completed with an error.
+        bool is_error() const;
+
+        /// Returns the complete response from CGI.
+        std::unique_ptr<Response> response() &&;
+
+    private:
         enum class Expect
         {
             Headers,
             Body,
         };
 
-        virtual ~CGIReadTask() override;
+        const static size_t       MAX_CONTENT = 1000000;
 
-        // Construct env, spawn cgi
-        CGIReadTask(int read_end, const Config& config, Child&& pid);
-
-        CGIReadTask(const CGIReadTask&) = delete;
-        CGIReadTask(CGIReadTask&&) = default;
-
-        CGIReadTask& operator=(const CGIReadTask&) = delete;
-        CGIReadTask& operator=(CGIReadTask&&) = default;
-
-        // Read cgi output into response body, enqueue ServerSendResponseTask
-        virtual void run() override;
-
-        // For error state checking
-        bool is_error() const;
-
-        /// Returns the complete response from CGI.
-        std::unique_ptr<Response> response() &&;
-
-        // Abort override
-        void abort() override;
-
-        std::optional<Seconds> expire_time() const override;
-
-    private:
         Expect                    _expect = Expect::Headers;
         Reader                    _reader = Reader(4096UL);
         std::unique_ptr<Response> _response = std::make_unique<Response>(Status::OK);
         Child                     _pid;
         bool                      _is_error = false;
-        const static size_t       _upload_limit = 1000000;
         Seconds                   _expire_time;
 
         void read_headers();
